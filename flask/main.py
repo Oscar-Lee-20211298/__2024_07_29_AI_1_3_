1f1740c2761f5d17b38b1eb7fe7bf332691b036a
Oscar-Lee-20211298 <djkhalod@gmail.com>
2024-10-09 13:24:23 +0000

    1

--- flask/main.py
+++ flask/main.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>星期三 lee,您好!</h1>"
+    return '''<h1>這是python的project</h1>
+            <p>這是在codespace環境開發的</p>
+        '''
